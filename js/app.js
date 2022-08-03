ea4fecd9ce302bb461d1cd4ae752f261c6d5dd38
hein khant <heinkhantzaw14720@gmail.com>
2022-08-03 17:59:39 +0630

    mhDay2

--- js/app.js
+++ js/app.js
@@ -1,3 +1,31 @@
 $(document).ready(function(){
-    console.log("I am working.");
+    // console.log("I am working.");
+
+
+/* Start Header Section */
+
+/* Start Banner Section */
+
+/* End Banner Section */
+
+
+/* End Header Section */
+
+
+
+// Start Info Section
+$(window).scroll(function(){
+    let getscrollpoint = $(this).scrollTop();
+    // console.log(getscrollpoint);
+
+    if(getscrollpoint >= 400){
+        $(".infotexts").addClass("fromlefts");
+        $(".infopics").addClass("fromrights");
+    }else{
+        $(".infotexts").removeClass("fromlefts");
+        $(".infopics").removeClass("fromrights");
+    }
+
+})
+// End Info Section
 });
