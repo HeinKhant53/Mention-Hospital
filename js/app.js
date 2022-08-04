252272ff2f1ecd0432f2e636af0f86439edc0761
hein khant <heinkhantzaw14720@gmail.com>
2022-08-04 11:03:08 +0630

    mhDay4

--- js/app.js
+++ js/app.js
@@ -28,4 +28,33 @@ $(window).scroll(function(){
 
 })
 // End Info Section
+
+
+// Start  Premises Section
+$("#lightslider").lightSlider({
+    item:4,
+    auto:true,
+    loop:true,
+    slideMove:1,
+    speed:600
+}).play();
+
+// End  Premises Section
+
+// Start Pricing Section
+$(window).scroll(function(){
+    let getscroll = $(this).scrollTop();
+    // console.log(getscroll);
+
+    if(getscroll >= 2450){
+        $(".cardones").addClass("movelefts");
+        $(".cardtwos").addClass("movebottoms");
+        $(".cardthrees").addClass("moverights");
+    }else{
+        $(".cardones").removeClass("movelefts");
+        $(".cardtwos").removeClass("movebottoms");
+        $(".cardthrees").removeClass("moverights");
+    }
+})
+// End Pricing Section
 });
